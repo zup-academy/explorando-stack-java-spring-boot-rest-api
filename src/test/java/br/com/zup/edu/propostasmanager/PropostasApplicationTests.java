42f8de04ecc3b5a898ddfa32738870cee5bb5148
jordisilvazup <jordi.silva@zup.com.br>
2022-12-01 08:22:14 -0300

    stk-cli: create app initial commit.

---
+++ src/test/java/br/com/zup/edu/propostasmanager/PropostasApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.zup.edu.propostasmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PropostasApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
